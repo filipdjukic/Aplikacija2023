59745a6ea3db9e0380916b43e4ee4773118646c5
Filip Djukic <fdjukic01@gmail.com>
2023-10-01 16:06:08 +0200

    Create the administrator table service

---
+++ src/services/administrator/administrator.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Administrator } from 'entities/administrator.entity';
+import { FindOneOptions, Repository } from 'typeorm';
+
+@Injectable()
+export class AdministratorService {
+    constructor(
+        @InjectRepository(Administrator)
+        private readonly administrator: Repository<Administrator>
+    ) { }
+
+    getAll(): Promise<Administrator[]> {
+        return this.administrator.find();
+    }
+
+
+    //Old piece of code
+    /*getById(id: number): Promise<Administrator> {
+        return this.administrator.findOne(id);
+    }*/
+
+    //updated piece of code
+    getById(id: number): Promise<Administrator> {
+        return this.administrator.findOne({ where: { id } } as FindOneOptions<Administrator>);
+    }    
+    
+}
